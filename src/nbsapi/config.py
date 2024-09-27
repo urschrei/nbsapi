008ee556072eb693321163611372f512be7215c6
Stephan Hügel <shugel@tcd.ie>
2024-09-27 11:35:33 +0100

    Ensure that .env isn't present on prod, sql echo off

--- src/nbsapi/config.py
+++ src/nbsapi/config.py
@@ -3,11 +3,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     database_url: str
-    echo_sql: bool = True
+    echo_sql: bool = False
     test: bool = False
     project_name: str = "nbsapi"
     oauth_token_secret: str = "secret"
     model_config = SettingsConfigDict(env_file=".env")
+    # env vars will always override settings from .env
 
 
 settings = Settings()  # type: ignore
